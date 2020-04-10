78eaf0524d1a597354360c3779159c44c0742cf7
ghigbie <gehigbie@gmail.com>
2020-04-09 22:53:10 -0700

    Import chalk

--- note-taking/app.js
+++ note-taking/app.js
@@ -1,7 +1,13 @@
 const utils = require('./utils');
 const getNotes = require('./notes');
+const validator = require('validator');
+const chalk = require('chalk');
 
 const sum = utils.add(4, 5);
+const blue = chalk.bold.bgBlue;
+const red = chalk.bold.bgRed;
+const yellow = chalk.yellow;
 
-console.log(`Message: ${utils.message}, Sum: ${sum}`);
-console.log(getNotes());
+console.log(blue(`Message: ${utils.message}, Sum: ${sum}`));
+console.log(yellow(`Validator: ${validator.isURL('ww.amazon.com')}`))
+console.log(red(getNotes()));
