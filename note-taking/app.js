43600a9d5d726d5bbd3d3c9c0b8303f3486e11ba
ghigbie <gehigbie@gmail.com>
2020-04-09 22:55:04 -0700

    Add more chalk

--- note-taking/app.js
+++ note-taking/app.js
@@ -7,7 +7,9 @@ const sum = utils.add(4, 5);
 const blue = chalk.bold.bgBlue;
 const red = chalk.bold.bgRed;
 const yellow = chalk.yellow;
+const green = chalk.bold.bgGreen;
 
 console.log(blue(`Message: ${utils.message}, Sum: ${sum}`));
 console.log(yellow(`Validator: ${validator.isURL('ww.amazon.com')}`))
 console.log(red(getNotes()));
+console.log(green('Success!!!'))
