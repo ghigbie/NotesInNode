1632a59ce2be7650c605b1485de43cd43f1c8cb9
ghigbie <gehigbie@gmail.com>
2020-04-09 22:20:30 -0700

    Edit app structure

--- note-taking/app.js
+++ note-taking/app.js
@@ -1,5 +1,7 @@
 const utils = require('./utils');
+const getNotes = require('./notes');
+
 const sum = utils.add(4, 5);
 
 console.log(`Message: ${utils.message}, Sum: ${sum}`);
-console.log(utils.getNotes());
+console.log(getNotes());
--- note-taking/utils.js
+++ note-taking/utils.js
@@ -1,11 +1,9 @@
 const message = "Yo! NodeJS is awesome!!!";
 const add = (a, b) => a+b;
-const getNotes = () => {
-    return "Your notes...";
-}
+
 
 module.exports = {
     message,
     add, 
-    getNotes
+
 }
