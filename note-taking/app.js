95d3ecf02c7e4aed7a1a8e2ab87a41bb1827442d
ghigbie <gehigbie@gmail.com>
2020-04-09 21:53:12 -0700

    Edit callback

--- note-taking/app.js
+++ note-taking/app.js
@@ -1,3 +1,16 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.tx', `This file was created by Node.JS`);
+fs.readdir('.', (err, files) => {
+    files.forEach(file => {
+        console.log(file);
+        if(file = 'notes.txt'){
+            fs.appendFileSync('notes.txt', `You found this file!!!`)
+        }else{
+            fs.writeFileSync('notes.txt', 'This file was created by Node.JS for fun!')
+            fs.appendFileSync('notes.txt', `It took a bit, but you found this file!!!`)
+        }
+    });
+    if(err){
+        console.log('There was an error', err)
+    }
+});
