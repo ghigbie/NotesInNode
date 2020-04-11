adda140a450800e9c06815e7f20e50e6ea99eb20
ghigbie <gehigbie@gmail.com>
2020-04-10 22:50:44 -0700

    isntall yargs

--- note-taking/app.js
+++ note-taking/app.js
@@ -3,13 +3,15 @@ const getNotes = require('./notes');
 const validator = require('validator');
 const chalk = require('chalk');
 
-const sum = utils.add(4, 5);
-const blue = chalk.bold.bgBlue;
-const red = chalk.bold.bgRed;
-const yellow = chalk.yellow;
-const green = chalk.bold.bgGreen;
 
-console.log(blue(`Message: ${utils.message}, Sum: ${sum}`));
-console.log(yellow(`Validator: ${validator.isURL('ww.amazon.com')}`))
-console.log(red(getNotes()));
-console.log(green('Success!!!'))
+
+const command = process.argv[2];
+
+if (command === 'add'){
+    console.log(utils.green('Adding note'));
+}else if(command === 'remove'){
+    console.log(utils.red('Removing note'));
+}
+
+
+console.log(process.argv);
--- note-taking/utils.js
+++ note-taking/utils.js
@@ -1,9 +1,16 @@
+const chalk = require('chalk')
+
 const message = "Yo! NodeJS is awesome!!!";
 const add = (a, b) => a+b;
 
+const green = chalk.green.bold.bgBlue;
+const red = chalk.red.bold.bgYellow;
+const blue = chalk.blue.bold;
 
 module.exports = {
     message,
     add, 
-
+    green,
+    red,
+    blue
 }
