795460aacadedd1cae3e79983c881d3553d4afa7
ghigbie <gehigbie@gmail.com>
2020-04-12 18:06:58 -0700

    Add read note funciton

--- note-taking/notes.js
+++ note-taking/notes.js
@@ -7,9 +7,9 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title);
 
-    if (duplicateNotes.length === 0) {
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -43,6 +43,17 @@ const listNotes = () => {
     })
 }
 
+const readNote = title => {
+    const notes = loadNotes()
+    const note =  notes.find(note => note.title === title);
+    if(note){
+        console.log(chalk.green.inverse(note.title));
+        console.log(note.body)
+    }else{
+        console.log(chalk.red.inverse('No note found'));
+    }
+}
+
 const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
@@ -59,8 +70,9 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
-    addNote: addNote,
-    removeNote: removeNote,
-    listNotes: listNotes
+    getNotes,
+    addNote,
+    removeNote,
+    listNotes,
+    readNote
 }
